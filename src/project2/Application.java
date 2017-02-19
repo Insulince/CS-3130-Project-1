1326792cf9a6c0c6fb178864eaceb0c9d4cfd54a
Justin <justinreusnow@gmail.com>
2017-02-19 16:23:06 -0600

    progress. Binary isn't working. Its close though.

--- src/project2/Application.java
+++ src/project2/Application.java
@@ -2,25 +2,43 @@ package project2;
 
 import project2.sorter.insertion.InsertionSorter;
 
-public class Application {
-    public static void main(String[] args) {
-        Integer[] data = generateData();
+import java.util.Arrays;
 
-        displayData(data);
+public class Application {
+    public static void main(String[] args) throws Exception {
+        int[] data1 = generateData();
+        int[] data2 = Arrays.copyOf(data1, data1.length);
+
+        if (!Arrays.toString(data1).equals(Arrays.toString(data2))) {
+            throw new Exception("Could not duplicate integer array, thus a valid performance comparison cannot be made!");
+        }
+
+        displayData(data1);
 
         InsertionSorter insertionSorter = new InsertionSorter();
 
-        data = insertionSorter.linearInsertionSort(data);
+        data1 = insertionSorter.linearInsertionSort(data1);
 
-        displayData(data);
+        displayData(data1);
+
+        displayData(data2);
+
+        insertionSorter.binaryInsertionSort(data2);
+
+        displayData(data2);
+
+        System.out.println(Arrays.toString(data1).equals(Arrays.toString(data2)));
+
+//        System.out.println(insertionSorter.linearAssignments);
+//        System.out.println(insertionSorter.linearComparisons);
     }
 
-    private static Integer[] generateData() {
+    private static int[] generateData() {
         final int AMOUNT = 1000;
         final int LOWER_BOUND = 1;
         final int UPPER_BOUND = 1000;
 
-        Integer[] ret = new Integer[AMOUNT];
+        int[] ret = new int[AMOUNT];
 
         for (int i = 0; i < AMOUNT; i++) {
             ret[i] = (int) (LOWER_BOUND + Math.random() * (UPPER_BOUND - LOWER_BOUND + 1));
@@ -30,11 +48,11 @@ public class Application {
         return ret;
     }
 
-    private static void displayData(final Integer[] data) {
+    private static void displayData(final int[] data) {
         System.out.println(formatData(data));
     }
 
-    private static String formatData(final Integer[] data) {
+    private static String formatData(final int[] data) {
         final int NUM_ROWS = 50;
         final int NUM_COLUIMNS = 20;
 
@@ -42,7 +60,7 @@ public class Application {
         String output;
 
         output = "================================================= ARRAY =================================================\n";
-        output += "  +   {00} {01} {02} {03} {04} {05} {06} {07} {08} {09} {10} {11} {12} {13} {14} {15} {16} {17} {18} {19}\n";
+        output += "INDEX {00} {01} {02} {03} {04} {05} {06} {07} {08} {09} {10} {11} {12} {13} {14} {15} {16} {17} {18} {19}\n";
         for (int rowNumber = 0; rowNumber < NUM_ROWS; rowNumber++) {
             String rowCounterAsString = String.valueOf(rowCounter);
             int lengthOfRowCounterString = rowCounterAsString.length();
--- src/project2/sorter/insertion/InsertionSorter.java
+++ src/project2/sorter/insertion/InsertionSorter.java
@@ -1,15 +1,15 @@
 package project2.sorter.insertion;
 
 public class InsertionSorter {
-    public Integer[] linearInsertionSort(final Integer[] data) {
+    public int[] linearInsertionSort(final int[] data) {
         for (int currentIntegerIndex = 1; currentIntegerIndex < data.length; currentIntegerIndex++) {
-            //Select next Integer.
-            Integer currentInteger = data[currentIntegerIndex];
+            //Select next int.
+            int currentInteger = data[currentIntegerIndex];
 
             //Compare with previous Integers.
             int insertionIndex = 0;
             for (int candidateInsertionIntegerIndex = currentIntegerIndex - 1; candidateInsertionIntegerIndex >= 0; candidateInsertionIntegerIndex--) {
-                Integer candidateInsertionInteger = data[candidateInsertionIntegerIndex];
+                int candidateInsertionInteger = data[candidateInsertionIntegerIndex];
                 if (currentInteger > candidateInsertionInteger) {
                     insertionIndex = candidateInsertionIntegerIndex + 1;
                     break;
@@ -19,36 +19,59 @@ public class InsertionSorter {
             //Shift affected Integers.
             for (int indexToBeShiftedInto = currentIntegerIndex; indexToBeShiftedInto > insertionIndex; indexToBeShiftedInto--) {
                 int indexToBeShiftedFrom = indexToBeShiftedInto - 1;
-                Integer currentlyShiftingInteger = data[indexToBeShiftedFrom];
+                int currentlyShiftingInteger = data[indexToBeShiftedFrom];
                 data[indexToBeShiftedInto] = currentlyShiftingInteger;
             }
 
-            //Insert current Integer.
+            //Insert current int.
             data[insertionIndex] = currentInteger;
         }
 
         return data;
     }
 
-    public Integer[] binaryInsertionSort(final Integer[] data) {
+    public int[] binaryInsertionSort(final int[] data) {
         for (int currentIntegerIndex = 1; currentIntegerIndex < data.length; currentIntegerIndex++) {
-            //Select next Integer.
-            Integer currentInteger = data[currentIntegerIndex];
-
+            //Select next int.
+            int currentInteger = data[currentIntegerIndex];
+            
             //Compare with previous Integers.
-            int insertionIndex = 0;
-            for (int candidateInsertionIntegerIndex = currentIntegerIndex - 1; candidateInsertionIntegerIndex >= 0; candidateInsertionIntegerIndex--) {
-                Integer candidateInsertionInteger = data[candidateInsertionIntegerIndex];
-                if (currentInteger > candidateInsertionInteger) {
-                    insertionIndex = candidateInsertionIntegerIndex + 1;
+            int insertionIndex;
+            int lower = 0;
+            int upper = currentIntegerIndex;
+            int range = upper - lower;
+            int focusIndex = range / 2 + lower;
+
+            while (upper > lower) {
+                if (currentInteger == data[focusIndex]) {
                     break;
+                } else if (currentInteger < data[focusIndex]) {
+                    lower = lower;
+                    upper = focusIndex - 1;
+                    range = upper - lower;
+                    focusIndex = range / 2 + lower;
+                } else if (currentInteger > data[focusIndex]) {
+                    lower = focusIndex + 1;
+                    upper = upper;
+                    range = upper - lower;
+                    focusIndex = range / 2 + lower;
                 }
             }
 
-            //Shift affected Integers.
-            //implement
+            if (currentInteger >= data[focusIndex]) {
+                insertionIndex = focusIndex + 1;
+            } else {
+                insertionIndex = focusIndex;
+            }
 
-            //Insert current Integer.
+            //Shift affected Integers.
+            for (int indexToBeShiftedInto = currentIntegerIndex; indexToBeShiftedInto > insertionIndex; indexToBeShiftedInto--) {
+                int indexToBeShiftedFrom = indexToBeShiftedInto - 1;
+                int currentlyShiftingInteger = data[indexToBeShiftedFrom];
+                data[indexToBeShiftedInto] = currentlyShiftingInteger;
+            }
+
+            //Insert current int.
             data[insertionIndex] = currentInteger;
         }
 
